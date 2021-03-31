9d160a93575e133ad8c8901a893d3f6c0b18de25
EvaluacionKBGV <81711480+EvaluacionKBGV@users.noreply.github.com>
2021-03-31 14:33:51 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,4 +1,7 @@
-firebase.initializeApp({
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
     apiKey: "AIzaSyCVP1FMoB46lkULgBvz2_nM9K0mma9wRcA",
     authDomain: "evaluacionkebgv.firebaseapp.com",
     projectId: "evaluacionkebgv",
@@ -6,4 +9,8 @@ firebase.initializeApp({
     messagingSenderId: "485263068724",
     appId: "1:485263068724:web:65cda326935e473d870461",
     measurementId: "G-Y25E16R32V"
-});
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
