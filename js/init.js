1d13902697bcfbb9a00d06e3766863c80b0c0695
EvaluacionKBGV <81711480+EvaluacionKBGV@users.noreply.github.com>
2021-04-12 15:49:08 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCVP1FMoB46lkULgBvz2_nM9K0mma9wRcA",
+    authDomain: "evaluacionkebgv.firebaseapp.com",
+    projectId: "evaluacionkebgv",
+    storageBucket: "evaluacionkebgv.appspot.com",
+    messagingSenderId: "485263068724",
+    appId: "1:485263068724:web:65cda326935e473d870461",
+    measurementId: "G-Y25E16R32V"
+});
